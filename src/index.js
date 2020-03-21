6356baf9e41902485402cd898a4cc9cfdc843b2c
achuang <achuang@achuangdeMacBook-Pro.local>
2020-03-21 18:36:01 +0800

    feat: 增加展示功能

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-import PART1 from "./bar-chart/part1";
-import PART2 from "./bar-chart/part2";
+import part1 from "./bar-chart/part1";
+import part2 from "./bar-chart/part2";
+import part3Render from "./bar-chart/part3";
+import part4 from "./bar-chart/part4";
 
 
-document.body.append(PART2)
+
+function render(target) {
+    const nodeMap = { part1, part2, part4 }
+    if (target === 3) {
+        part3Render()
+    } else {
+        $('content').appendChild(nodeMap[`part${target}`])
+
+    }
+}
+
+function $(id) {
+    return document.getElementById(id)
+}
+document.addEventListener('DOMContentLoaded', function () {
+    const btnWrapper = $('barChart')
+    btnWrapper.addEventListener('click', function (e) {
+        const { tagName, dataset } = e.target
+        tagName === 'BUTTON' && render(+dataset.target)
+
+    })
+})
+
+
