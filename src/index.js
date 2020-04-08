f1e90b92c0a4446e516a073529563419702415ea
shengchuang <shengchuang@sutpc.com>
2020-04-08 20:17:35 +0800

    feat: pie-second

--- src/index.js
+++ src/index.js
@@ -2,32 +2,12 @@ import part1 from "./bar-chart/part1";
 import part2 from "./bar-chart/part2";
 import part3Render from "./bar-chart/part3";
 import part4 from "./bar-chart/part4";
-
 import flow from './flow/index'
-$('content').appendChild(flow)
+import PieSecond from "./pie-second";
 
-
-
-function render(target) {
-    const nodeMap = { part1, part2, part4 }
-    if (target === 3) {
-        part3Render()
-    } else {
-        $('content').appendChild(nodeMap[`part${target}`])
-
-    }
-}
-
-function $(id) {
-    return document.getElementById(id)
-}
 document.addEventListener('DOMContentLoaded', function () {
-    const btnWrapper = $('barChart')
-    btnWrapper.addEventListener('click', function (e) {
-        const { tagName, dataset } = e.target
-        tagName === 'BUTTON' && render(+dataset.target)
-
-    })
+    const wrapper = document.getElementById('content')
+    PieSecond.render(wrapper)
 })
 
 
